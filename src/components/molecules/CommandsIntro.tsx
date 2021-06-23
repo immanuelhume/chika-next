dcfe31c7b54f187ef79bc036ec8f0783dda2e343
junyi <47441677+immanuelhume@users.noreply.github.com>
2021-06-23 23:59:18 +0800

    style: text align issues

--- src/components/molecules/CommandsIntro.tsx
+++ src/components/molecules/CommandsIntro.tsx
@@ -4,7 +4,11 @@ import React from 'react';
 export function CommandsIntro() {
   return (
     <VStack>
-      <Box color="whiteAlpha.800">
+      <Box
+        color="whiteAlpha.800"
+        w="full"
+        textAlign={{ base: `left`, md: `center` }}
+      >
         Chika&apos;s default prefix is{` `}
         <Text fontWeight="bold" display="inline" color="whiteAlpha.800">
           ck;
@@ -12,7 +16,11 @@ export function CommandsIntro() {
         {` `}
         and is customizable per-server.
       </Box>
-      <Box color="whiteAlpha.800">
+      <Box
+        color="whiteAlpha.800"
+        w="full"
+        textAlign={{ base: `left`, md: `center` }}
+      >
         A <Code colorScheme="pink">pink</Code> tag indicates a required
         argument, and a <Code>gray</Code> tag indicates an optional one.
       </Box>
